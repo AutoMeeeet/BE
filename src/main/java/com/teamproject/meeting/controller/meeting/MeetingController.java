67e42031a452781c4f6556d3119184248d22af55
naseokhoo <nahoo1202@naver.com>
2026-01-06 01:16:06 +0900

    컨트롤러리텍토리

--- src/main/java/com/teamproject/meeting/controller/meeting/MeetingController.java
+++ src/main/java/com/teamproject/meeting/controller/meeting/MeetingController.java
@@ -1,9 +1,14 @@
 package com.teamproject.meeting.controller.meeting;
 
+import com.teamproject.meeting.dto.meeting.MeetingListResDto;
+import com.teamproject.meeting.enums.meeting.MeetingState;
+import com.teamproject.meeting.service.meeting.MeetingListService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.teamproject.meeting.dto.meeting.CreateMeetingDto;
@@ -14,14 +19,17 @@ import com.teamproject.meeting.service.meeting.MeetingService;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 
+import java.util.List;
+
 @RestController
 public class MeetingController {
 
 	private final MeetingService meetingService;
-	
-	public MeetingController(MeetingService meetingService) {
+    private final MeetingListService meetingListService;
+	public MeetingController(MeetingService meetingService, MeetingListService meetingListService) {
 		this.meetingService = meetingService;
-	}
+        this.meetingListService = meetingListService;
+    }
 	
 	// Meeting 생성
 	@PostMapping("/createmeeting")
@@ -33,4 +41,13 @@ public class MeetingController {
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(new UrlResponse("Meeting 생성 성공", "/meetings/" + token));
 	}
+     //MeetingList
+    @GetMapping("/meetinglist")
+    public List<MeetingListResDto> getMeetings(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @RequestParam(required = false) MeetingState state
+    ) {
+
+        return meetingListService.getMeetings(user.getUserId(), state);
+    }
 }
